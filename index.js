8f737103aeee546685b4eeaaaf707ed44e35867c
Cam Holloway <hollowprince@outlook.com>
2018-05-14 18:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+ /*
+ Learn IDE required changes to be made, name variable changed to my own
+ */
